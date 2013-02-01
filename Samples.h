924ecb908efb187c2988508af89bb33fc188014e
Daniel Esteban Nombela <dani@hellacoders.com>
2013-02-01 19:21:26 +0100

    Samples bugfix

--- Samples.h
+++ Samples.h
@@ -11,6 +11,6 @@ const byte numSamples = 9,
 
 const String sampleNames[numSamples] = {"BASSDRUM", "SNARE", "STICK", "HI-HAT CLOSED", "HI-HAT OPEN", "HIGH TOM", "FLOOR TOM", "RAID", "CRASH"};
 
-extern PROGMEM const char * samples[numSamples];
+extern const char * samples[numSamples];
 
 #endif
